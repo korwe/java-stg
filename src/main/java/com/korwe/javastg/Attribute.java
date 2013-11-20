75297ddcd61468343290c8165e409a4f4f349cbe
Tjad Clark <tjad.clark@korwe.com>
2013-11-20 12:23:32 +0200

    Added build script and gitignore

---
+++ src/main/java/com/korwe/javastg/Attribute.java
@@ -0,0 +1,52 @@
+package com.korwe.javastg;
+
+/**
+ * @author <a href="mailto:tjad.clark@korwe.com>Tjad Clark</a>
+ */
+public class Attribute {
+    private String name;
+    private String type;
+    private AccessModifier accessModifier;
+    private boolean hasGetter;
+    private boolean hasSetter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public AccessModifier getAccessModifier() {
+        return accessModifier;
+    }
+
+    public void setAccessModifier(AccessModifier accessModifier) {
+        this.accessModifier = accessModifier;
+    }
+
+    public boolean isHasGetter() {
+        return hasGetter;
+    }
+
+    public void setHasGetter(boolean hasGetter) {
+        this.hasGetter = hasGetter;
+    }
+
+    public boolean isHasSetter() {
+        return hasSetter;
+    }
+
+    public void setHasSetter(boolean hasSetter) {
+        this.hasSetter = hasSetter;
+    }
+}
