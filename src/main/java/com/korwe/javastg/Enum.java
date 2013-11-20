a50f10149e6acb26c87d42a235e0a76f8c82daf1
Tjad Clark <tjad.clark@korwe.com>
2013-11-20 14:12:38 +0200

    Added basic container for Enum ReferenceType

---
+++ src/main/java/com/korwe/javastg/Enum.java
@@ -0,0 +1,36 @@
+package com.korwe.javastg;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:tjad.clark@korwe.com>Tjad Clark</a>
+ */
+public class Enum extends ReferenceType{
+    private List<String> values;
+    private List<Attribute> attributes;
+    private List<Method> methods;
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public void setValues(List<String> values) {
+        this.values = values;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+    public List<Method> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<Method> methods) {
+        this.methods = methods;
+    }
+}
