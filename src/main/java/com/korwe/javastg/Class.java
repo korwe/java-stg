ec7462940396b0aaab100ac78128f881bb91d430
Tjad Clark <tjad.clark@korwe.com>
2013-11-20 13:26:06 +0200

    Added basic class container

---
+++ src/main/java/com/korwe/javastg/Class.java
@@ -0,0 +1,72 @@
+package com.korwe.javastg;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:tjad.clark@korwe.com>Tjad Clark</a>
+ */
+public class Class {
+    private String name;
+    private String packageName;
+    private AccessModifier accessModifier;
+    private Class superClass;
+    private List<Attribute> attributes;
+    private List<Method> methods;
+    private boolean isAbstract;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public AccessModifier getAccessModifier() {
+        return accessModifier;
+    }
+
+    public void setAccessModifier(AccessModifier accessModifier) {
+        this.accessModifier = accessModifier;
+    }
+
+    public Class getSuperClass() {
+        return superClass;
+    }
+
+    public void setSuperClass(Class superClass) {
+        this.superClass = superClass;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+    public List<Method> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<Method> methods) {
+        this.methods = methods;
+    }
+
+    public boolean isAbstract() {
+        return isAbstract;
+    }
+
+    public void setAbstract(boolean anAbstract) {
+        isAbstract = anAbstract;
+    }
+}
