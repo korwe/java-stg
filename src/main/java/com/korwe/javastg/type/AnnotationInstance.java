279c8b13b62a55395025d9d77ac2fec2691ac600
Tjad Clark <tjad.clark@korwe.com>
2013-11-22 11:57:11 +0200

    Added support for annotation attributes and default annotation attributes

--- src/main/java/com/korwe/javastg/type/AnnotationInstance.java
+++ src/main/java/com/korwe/javastg/type/AnnotationInstance.java
@@ -1,14 +1,23 @@
 package com.korwe.javastg.type;
 
+import com.korwe.javastg.value.TypeDefinitionValue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:tjad.clark@korwe.com>Tjad Clark</a>
  */
 public class AnnotationInstance {
     private Annotation annotation;
+    private Map<String, TypeDefinitionValue> values = new HashMap<>();
 
     public AnnotationInstance(Annotation annotation){
         this.annotation = annotation;
+    }
+
+    public TypeDefinitionValue setValue(String name, TypeDefinitionValue value){
+        return values.put(name, value);
     }
 
     public Annotation getAnnotation() {
@@ -17,5 +26,9 @@ public class AnnotationInstance {
 
     public void setAnnotation(Annotation annotation) {
         this.annotation = annotation;
+    }
+
+    public Map<String, TypeDefinitionValue> getValues() {
+        return values;
     }
 }
